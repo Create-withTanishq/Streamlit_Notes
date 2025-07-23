563e18e22926088fc35795612bc2eb726d713095
Create-withTanishq <tanishqsaha2004@gmail.com>
2025-07-23 13:25:05 +0530

    added texts.py

--- Basics/texts.py
+++ Basics/texts.py
@@ -1,3 +1,23 @@
 import streamlit as st
+import os
 
-st.write("Hello World 1234")
+st.title("This is the Title")
+st.header("This is the Heading")
+st.subheader("This is the Subheader")
+
+st.text("plain text")
+
+st.markdown("## Markdown ##")
+
+st.caption("This is the Caption")
+
+st.divider()
+
+st.code('''def show(id :int):
+            print(id) ''',
+        language = "python")
+
+st.divider()
+
+st.subheader("Image Display")
+st.image("Basics/static/porsche.jpg")
